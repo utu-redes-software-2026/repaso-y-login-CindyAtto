b9d15c645fb2081085f35fd765d1155709ec6f4b
CindyAtto <leti115@gmail.com>
2026-05-06 15:57:52 -0300

    Ruta protegida y panel privado

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::middleware(['auth'])->get('/panel', function () {
+    return view('panel');
+});
 
 require __DIR__.'/auth.php';
